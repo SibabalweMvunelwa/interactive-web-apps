350ebb709682fdb0f4677b138e1480edecbd6937
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-03-28 17:46:49 +0200

    Vers1.2 - added Defer attribute. Sorted issue.

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,6 +1,6 @@
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
-const STEP_AMOUNT = 5
+const STEP_AMOUNT = 5;
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
