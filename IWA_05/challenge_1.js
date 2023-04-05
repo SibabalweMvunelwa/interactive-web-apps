a7452b62023e3b304799f5ff56c409b0fc8bfa97
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-04-05 12:40:58 +0200

    IWA_05 : Revision (Working)

--- IWA_05/challenge_1.js
+++ IWA_05/challenge_1.js
@@ -1,30 +1,31 @@
 const WARNING = 'Free shipping only applies to single customer orders'
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-const NONE_SELECTED = '0'
+const NONE_SELECTED = 0
 
 let customers = 1
-let location = RSA
+let locationA = 'RSA'
 let currency = null
 let shipping = null
 
-if ( location == RSA ) { 
+if ( locationA == 'RSA' ) { 
     shipping = 400 && currency == 'R' 
 }
 
-if ( location == NAM ) {
+if ( locationA == 'NAM' ) {
 shipping = 600 
 } else { shipping = 800 
 }
 
 let shoes = 300 * 1
 let toys = 100 * 5
-let shirts = 150 * 'NONE_SELECTED'
+let shirts = 150 * NONE_SELECTED
 let batteries = 35 * 2
-let pens = 5 * 'NONE_SELECTED' 
+let pens = 5 * NONE_SELECTED
 
 if ( shoes + toys + batteries + pens + shirts > 1000 ) 
 {
-	if ( ( location == NAM || location == RSA ) && ( customers = 1 ) )
+	if ( ( locationA == 'NAM' || locationA
+     == 'RSA' ) && ( customers = 1 ) )
     {
 		  shipping = 0
 		}
@@ -36,4 +37,4 @@ if ( shipping = 0 && customers !== 1 ) {
 }
 
 const price = currency + shoes + batteries + pens + shirts + shipping
-location == NK ? console.log(BANNED_WARNING) : console.log(price)
+locationA == 'NK' ? console.log(BANNED_WARNING) : console.log(price)
