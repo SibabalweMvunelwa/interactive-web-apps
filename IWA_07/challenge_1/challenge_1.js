1bcfaa6717d464ea5c8288b68ab3bb5b64bb3605
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-04-11 14:20:27 +0200

    IWA7: Challenge 1 (Complete)

---
+++ IWA_07/challenge_1/challenge_1.js
@@ -0,0 +1,7 @@
+const value = 3
+
+console.log(value + 4 + value)
+
+/* The value of the constant "value" was equal to a string,
+   meant that we were concatenating instead of adding the
+   values together. */ 
