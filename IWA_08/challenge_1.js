f6f927017c08df0e24790aac0500593b9e560434
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-04-17 15:25:52 +0200

    IWA_08: Added trimEnd() and " " to username

--- IWA_08/challenge_1.js
+++ IWA_08/challenge_1.js
@@ -27,7 +27,7 @@ const leo = {
 }
 
 const sarah = {
-	username: sarahName + sarahSurname,
+	username: sarahName.trimEnd(sarahName) + " " + sarahSurname,
 	age: 62,
 	accessId: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
 	balance: sarahBalance,
